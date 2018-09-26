29eda0d966e9bfe2658be8a6701ab2cee75324af
terpsihora96 <dzedaj996@gmail.com>
2018-09-26 09:46:19 +0200

    Parser changed.

--- parser.py
+++ parser.py
@@ -23,16 +23,21 @@ def p_goto_instruction(t):
                         | GOTO LPAR NUMBER RPAR'''
 
 def p_condition(t):
-    '''condition : E LTGT E'''
+    '''condition : var_num LTGT var_num'''
 
 def p_E(t):
-    '''E : E OPERATOR E
-        | LPAR E RPAR
-        | ID
-        | NUMBER'''
+    '''E : ID OPERATOR ID
+        | ID OPERATOR NUMBER
+        | NUMBER OPERATOR ID
+        | NUMBER OPERATOR NUMBER
+        | var_num'''
 
-def p_error(p):
-    print("Syntax error")
+def p_var_num(t):
+    '''var_num : NUMBER
+                | ID'''
+
+def p_error(t):
+    print("Syntax error.")
     exit(0)
 
 yacc.yacc()
